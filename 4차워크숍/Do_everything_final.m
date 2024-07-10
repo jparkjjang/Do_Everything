c37852429ea6fadc9423d662eb7d1d4db9c45e5c
LYJ <box1577@gmail.com>
2024-07-10 19:57:57 +0900

    4차

--- 4차워크숍/Do_everything_final.m
+++ 4차워크숍/Do_everything_final.m
@@ -19,6 +19,7 @@ cameraParams = cameraParameters('IntrinsicMatrix', intrinsicMatrix, ...
                                 'TangentialDistortion', tangentialDistortion, ...
                                 'ImageSize', imageSize);
 % 드론 객체 생성 및 연결
+global droneObj;
 droneObj = ryze();
 disp(['Battery Level: ' num2str(droneObj.BatteryLevel) '%']);
 
@@ -42,7 +43,7 @@ preview(cameraObj, hImage);
 takeoff(droneObj);
 
 
-function[x,y,z,t] = circle(radious, minr)
+function[x,y,z,t] = circle(radious, minr,maxr)
     global cameraObj;
     % 카메라 파라미터 설정
     focalLength = [957.1698 963.1543];
@@ -95,8 +96,24 @@ function[x,y,z,t] = circle(radious, minr)
     
     
     % 원 감지 (Hough 변환 사용)
-    [centers, radii] = imfindcircles(binaryImage, [minr 1000], 'ObjectPolarity', 'dark', 'Sensitivity', 0.95);
-    [maxValue, maxIndex] = max(radii);
+    [centers, radii] = imfindcircles(binaryImage, [50 1000], 'ObjectPolarity', 'dark', 'Sensitivity', 0.97);
+    
+    new_centers = [];
+    new_radii = [];
+    if length(centers(:,1)) == 1
+        new_centers = centers;
+        new_radii = radii;
+    else
+        for i= 1:length(centers(:,1))
+            if (340 <= centers(i,1)) && (centers(i,1) <= 630)
+                new_centers = [new_centers; centers(i,:)];
+                new_radii = [new_radii; radii(i)]
+            end
+        end
+    end
+    [maxValue, maxIndex] = max(new_radii);
+
+
     
     % 감지된 원 표시
     % imshow(image);
@@ -107,7 +124,7 @@ function[x,y,z,t] = circle(radious, minr)
     % 원 중심 좌표
     t = 1;
     try
-        circleCenter = centers(maxIndex, :);
+        circleCenter = new_centers(maxIndex, :);
     
     
     
@@ -155,9 +172,9 @@ function[x,y,z,t] = circle(radious, minr)
     end
 end
 
-function[rx, ry, rz,rt] = rr()
+function[rx, ry, rz,rt] = rr(rgp) %rgp==1 -> red// 2 -> green// 3 -> purple
     global cameraObj;
-
+    
     % 카메라 파라미터 설정
     focalLength = [957.1698 963.1543];
     principalPoint = [495.3551 394.4279];
@@ -177,25 +194,41 @@ function[rx, ry, rz,rt] = rr()
 
     % 사진 촬영
     image = snapshot(cameraObj);
-    
+
+
     % HSV 색상 공간으로 변환
     hsvImage  = rgb2hsv(image);
+
+    if rgp == 1
+        % 붉은색 범위 설정 (Hue 값 대략 0 - 0.05)
+        lower_red = [0, 0.25, 0.25];
+        upper_red = [0.05, 1, 1];
+        
+        % 붉은색 마스크 생성
+        mask = (hsvImage(:,:,1) >= lower_red(1) & hsvImage(:,:,1) <= upper_red(1)) & ...
+               (hsvImage(:,:,2) >= lower_red(2) & hsvImage(:,:,2) <= upper_red(2)) & ...
+               (hsvImage(:,:,3) >= lower_red(3) & hsvImage(:,:,3) <= upper_red(3));
     
-    % 붉은 색 범위 설정
-    lower_red1 = [0, 0.5, 0.5];
-    upper_red1 = [0.05, 1, 1];
-    lower_red2 = [0.95, 0.5, 0.5];
-    upper_red2 = [1, 1, 1];
+    elseif rgp == 2
+        % 초록색 범위 설정 (Hue 값 대략 0.25 - 0.45)
+        lower_green = [0.25, 0.2, 0.4];
+        upper_green = [0.45, 1, 1];
+        
+        % 초록색 마스크 생성
+        mask = (hsvImage(:,:,1) >= lower_green(1) & hsvImage(:,:,1) <= upper_green(1)) & ...
+               (hsvImage(:,:,2) >= lower_green(2) & hsvImage(:,:,2) <= upper_green(2)) & ...
+               (hsvImage(:,:,3) >= lower_green(3) & hsvImage(:,:,3) <= upper_green(3));
     
-    % 마스크 생성
-    mask1 = (hsvImage(:,:,1) >= lower_red1(1) & hsvImage(:,:,1) <= upper_red1(1)) & ...
-            (hsvImage(:,:,2) >= lower_red1(2) & hsvImage(:,:,2) <= upper_red1(2)) & ...
-            (hsvImage(:,:,3) >= lower_red1(3) & hsvImage(:,:,3) <= upper_red1(3));
-    mask2 = (hsvImage(:,:,1) >= lower_red2(1) & hsvImage(:,:,1) <= upper_red2(1)) & ...
-            (hsvImage(:,:,2) >= lower_red2(2) & hsvImage(:,:,2) <= upper_red2(2)) & ...
-            (hsvImage(:,:,3) >= lower_red2(3) & hsvImage(:,:,3) <= upper_red2(3));
-    redMask  = mask1 | mask2;
-    
+    elseif rgp == 3
+        % 보라색 범위 설정 (Hue 값 대략 0.75 - 0.85)
+        lower_purple = [0.65, 0.4, 0.25];
+        upper_purple = [0.85, 1, 1];
+        
+        % 보라색 마스크 생성
+        mask = (hsvImage(:,:,1) >= lower_purple(1) & hsvImage(:,:,1) <= upper_purple(1)) & ...
+               (hsvImage(:,:,2) >= lower_purple(2) & hsvImage(:,:,2) <= upper_purple(2)) & ...
+               (hsvImage(:,:,3) >= lower_purple(3) & hsvImage(:,:,3) <= upper_purple(3));
+    end
     
     %green
     % 초록색 범위 설정 (두 개의 허용치 적용)
@@ -242,7 +275,7 @@ function[rx, ry, rz,rt] = rr()
     rt = 1;
     try
         % 바이너리 마스크에서 영역 속성을 추출합니다.
-        statsRed = regionprops(redMask, 'Area', 'Centroid', 'BoundingBox');
+        statsRed = regionprops(mask, 'Area', 'Centroid', 'BoundingBox');
         
         % 가장 큰 사각형 찾기
         if ~isempty(statsRed)
@@ -280,58 +313,96 @@ function[rx, ry, rz,rt] = rr()
         rz = 0;
     end
 end
-moveup(droneObj,'Distance',0.4);
+
+function[] = md(x,y,z, val) %카메라 좌표계를 드론죄표계로 변경해줌/ 이동을 수월하게
+    global droneObj;
+    move(droneObj, [z,x,y],'Speed',val);
+end
+
+function[result] = try_circle(x, y, z, t,step)
+    if t == -1
+        result = 'fail';
+        fprintf('can not found circle \n');
+    end
+    switch step
+        case 1
+            if (2 < z)
+                result = 'back';
+                fprintf('원래 자리로 돌아가는 것을 추천!!');
+            end
+        case 2
+            if 6.7<z
+                result = 'back';
+                fprintf('원래 자리로 돌아가는 것을 추천!!');
+            end
+        case 3
+            if 4<z
+                result = 'back';
+                fprintf('원래 자리로 돌아가는 것을 추천!!');
+            end
+        case 4
+            if 4<z
+                result = 'back';
+                fprintf('원래 자리로 돌아가는 것을 추천!!');
+            end
+    end
+
+
+end
+
+function[result] = try_rac(x, y, z, t,step)
+    if t == -1
+        result = 'fail';
+        fprintf('can not found rac \n');
+    end
+    switch step
+        case 1
+            if (3 < z)
+                result = 'back';
+                fprintf('원래 자리로 돌아가는 것을 추천!!');
+            end
+        case 2
+            if 5<z
+                result = 'back';
+                fprintf('원래 자리로 돌아가는 것을 추천!!');
+            end
+        case 3
+            if 4<z
+                result = 'back';
+                fprintf('원래 자리로 돌아가는 것을 추천!!');
+            end
+        case 4
+            if 3<z
+                result = 'back';
+                fprintf('원래 자리로 돌아가는 것을 추천!!');
+            end
+    end
+end
+
+md(0.0, -0.3, -0.3, 0.5);
 
 %첫 인식
-[x, y, z, t] = circle(0.57, 50);
+[x, y, z, t] = circle(0.57, 50, 2000);
 
 while t == -1
-    [x, y, z, t] = circle(0.57, 50);
+    [x, y, z, t] = circle(0.57, 50, 2000);
     pause(0.5);
 end
 
 
-[x, y, z, t] = circle(0.57, 50);
-if x > 0.20
-    moveright(droneObj, 'Distance',x);
-elseif x < -0.2
-    moveleft(droneObj,'Distance',abs(x));
-end
+[x, y, z, t] = circle(0.57, 50, 2000);
+z = z ;
+y = y + 0.4;
+md(x,y,z,1);
 
-% if (0.10 < x) && (x < 0.20)
-%     moveright(droneObj, 'Distance',x + 0.20);
-%     moveleft(droneObj,'Distance',0.20);
-% elseif (x > -0.2) && (x<-0.1)
-%     moveleft(droneObj,'Distance',abs(x) + 0.20);
-%     moveright(droneObj,'Distance',0.20);
-% end
-movedown(droneObj,'Distance',0.2);
-moveforward(droneObj,'Distance', 1.8 ,'Speed', 1.0);
-
-
-
-
-[rx,ry,rz,rt] = rr();
-for i = 1:3
-    if rx > 0.20
-        moveright(droneObj, 'Distance',rx);
-    elseif rx < -0.21
-        moveleft(droneObj,'Distance',-rx);
-    end
-    moveforward(droneObj,'Distance',0.4);
-    [rx,ry,rz, rt] = rr();
-end
-
-if rx > 0.20
-    moveright(droneObj, 'Distance',rx);
-elseif rx < -0.21
-    moveleft(droneObj,'Distance',-rx);
-end
+[rx,ry,rz,rt] = rr(1);
+ry = ry + 0.5;
+rz = rz-0.8;
+md(rx,ry,rz,0.2)
 
 %------------------
-
-turn(droneObj, deg2rad(130));
-
+turn(droneObj, deg2rad(135));
+md(0,0,5,1);
 [x y z t] = circle(0.46, 40);
 if x > 0.20
     moveright(droneObj, 'Distance',x);
